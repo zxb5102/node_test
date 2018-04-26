36cb883f31b9b8ba631c1d596022b3b1b2ff6447
zxb5102 <zxb5102@163.com>
2018-04-26 13:55:11 +0800

    添加日志的支持

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// process.env.DEBUG='*,-app:*';
+process.env.DEBUG='sys';
 const Koa = require('koa');
 var Router = require('koa-router');
 const app = new Koa();
 var router = new Router();
+var debug = require('debug')('sys');
+debug('init');
 var server = require('koa-static')('.', {
     gzip: true,
     index: 'index.html'
 });
-
 router.get('/example/:id', (ctx, next) => {
     ctx.body =  `
         you input ${ctx.params.id}
